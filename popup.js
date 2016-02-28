234da3465ec1e30e67d83eccc8e127e56c855dad
Aashni Shah <contact@aashni.me>
2016-02-28 05:05:41 -0500

    mans best friend chrome plugin

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+      var imageResult = document.getElementById('image-result');
+      imageResult.width = 600;
+      imageResult.height = 600;
+      imageResult.src = 'http://loremflickr.com/600/600/dog';
+      imageResult.hidden = false;
+});
